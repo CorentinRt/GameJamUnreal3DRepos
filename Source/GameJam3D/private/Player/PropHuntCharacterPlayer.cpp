547a445151b2de091bd11fc653a4b025a176dc11
Corentinrmt <corentin.remot@gmail.com>
2024-09-30 14:42:31 +0200

    [ADD] Character script

---
+++ Source/GameJam3D/private/Player/PropHuntCharacterPlayer.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameJam3D/Public/Player/PropHuntCharacterPlayer.h"
+
+
+// Sets default values
+APropHuntCharacterPlayer::APropHuntCharacterPlayer()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APropHuntCharacterPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APropHuntCharacterPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APropHuntCharacterPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
