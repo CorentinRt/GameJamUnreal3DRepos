1257d21c3bd07ee39de3fb5116021288b000bf43
Corentinrmt <corentin.remot@gmail.com>
2024-09-30 14:49:19 +0200

    [ADD] create script game mode, controller

---
+++ Source/GameJam3D/private/GameMode/PropHuntGameMode.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameJam3D/Public/GameMode/PropHuntGameMode.h"
---
+++ Source/GameJam3D/public/GameMode/PropHuntGameMode.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PropHuntGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEJAM3D_API APropHuntGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+};
